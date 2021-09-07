27dc82ea52ecdd1d722dbcfcf98a8244c344eed6
Dylan.Mailloux <88984881+BooshBear@users.noreply.github.com>
2021-09-07 15:15:11 -0700

    Delete FSW-105 Week5 directory

--- FSW-105 Week5/Tests.js
+++
@@ -1,5 +0,0 @@
-function alphabetical(arr) {
-    return arr.sort();
-}
-console.log("---  Array Sort Extra Credt ---");
-console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"]));  // Output: ["by", "dog", "eaten", "family", "wolf"]
